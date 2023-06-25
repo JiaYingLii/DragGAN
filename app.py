51435e04f9e3d5786fff934c30cde286b35c2a2c
Jiaying Li <lijiaying@pjlab.org.cn>
2023-06-25 17:00:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -304,14 +304,14 @@ def main():
 
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--device', default='cuda')
+    # import argparse
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--device', default='cuda')
     # parser.add_argument('--share', action='store_true')
     # parser.add_argument('-p', '--port', default=None)
     # parser.add_argument('--ip', default=None)
-    args = parser.parse_args()
-    device = args.device
+    # args = parser.parse_args()
+    # device = args.device
     demo = main()
     print('Successfully loaded, starting gradio demo')
-    demo.queue(concurrency_count=1, max_size=20).launch(share=args.share, server_name=args.ip, server_port=args.port)
+    demo.queue(concurrency_count=1, max_size=20).launch()
